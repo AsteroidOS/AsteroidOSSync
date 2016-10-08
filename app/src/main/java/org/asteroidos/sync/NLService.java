293e92dfe5edcc7b895c9d9c48b6adb365269995
Florent Revest <revestflo@gmail.com>
2016-10-08 15:21:36 +0200

    NLService: adds an initial version of an android packageName <-> ion appIcon hashtable

--- app/src/main/java/org/asteroidos/sync/NLService.java
+++ app/src/main/java/org/asteroidos/sync/NLService.java
@@ -24,8 +24,13 @@ import android.content.IntentFilter;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class NLService extends NotificationListenerService {
     private NLServiceReceiver nlServiceReceiver;
+
+    private Map<String, String> iconFromPackage;
 
     @Override
     public void onCreate() {
@@ -34,6 +39,85 @@ public class NLService extends NotificationListenerService {
         IntentFilter filter = new IntentFilter();
         filter.addAction("org.asteroidos.sync.NOTIFICATION_LISTENER_SERVICE");
         registerReceiver(nlServiceReceiver, filter);
+
+        iconFromPackage = new Hashtable<>();
+        iconFromPackage.put("org.buffer.android", "logo-buffer");
+        iconFromPackage.put("com.android.chrome", "logo-chrome");
+        iconFromPackage.put("com.chrome.beta", "logo-chrome");
+        iconFromPackage.put("com.chrome.dev", "logo-chrome");
+        iconFromPackage.put("com.dropbox.android", "logo-dropbox");
+        iconFromPackage.put("com.facebook.groups", "logo-facebook");
+        iconFromPackage.put("com.facebook.work", "logo-facebook");
+        iconFromPackage.put("com.facebook.Mentions", "logo-facebook");
+        iconFromPackage.put("com.facebook.katana", "logo-facebook");
+        iconFromPackage.put("com.joelapenna.foursquared", "logo-foursquare");
+        iconFromPackage.put("com.google.android.googlequicksearchbox", "logo-google");
+        iconFromPackage.put("com.google.android.apps.plus", "logo-googleplus");
+        iconFromPackage.put("com.instagram.android", "logo-instagram");
+        iconFromPackage.put("com.instagram.layout", "logo-instagram");
+        iconFromPackage.put("com.instagram.boomerang", "logo-instagram");
+        iconFromPackage.put("com.linkedin.Coworkers", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.leap", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.android.salesnavigator", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.android.learning", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.recruiter", "logo-linkedin");
+        iconFromPackage.put("net.slideshare.mobile", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.pulse", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.android.jobs.jobseeker", "logo-linkedin");
+        iconFromPackage.put("com.linkedin.android", "logo-linkedin");
+        iconFromPackage.put("com.pinterest", "logo-pinterest");
+        iconFromPackage.put("com.scee.psxandroid", "logo-playstation");
+        iconFromPackage.put("com.playstation.mobilemessenger", "logo-playstation");
+        iconFromPackage.put("com.playstation.video", "logo-playstation");
+        iconFromPackage.put("com.playstation.remoteplay", "logo-playstation");
+        iconFromPackage.put("com.reddit.frontpage", "logo-reddit");
+        iconFromPackage.put("flipboard.app", "logo-rss");
+        iconFromPackage.put("com.noinnion.android.greader.reader", "logo-rss");
+        iconFromPackage.put("com.devhd.feedly", "logo-rss");
+        iconFromPackage.put("com.microsoft.office.lync15", "logo-skype");
+        iconFromPackage.put("com.skype.android.access", "logo-skype");
+        iconFromPackage.put("com.skype.raider", "logo-skype");
+        iconFromPackage.put("com.snapchat.android", "logo-snapchat");
+        iconFromPackage.put("com.valvesoftware.android.steam.community", "logo-steam");
+        iconFromPackage.put("com.tumblr", "logo-tumblr");
+        iconFromPackage.put("tv.twitch.android.app", "logo-twitch");
+        iconFromPackage.put("com.twitter.android", "logo-twitter");
+        iconFromPackage.put("com.vimeo.android.videoapp", "logo-vimeo");
+        iconFromPackage.put("com.whatsapp", "logo-whatsapp");
+        iconFromPackage.put("org.wordpress.android", "logo-wordpress");
+        iconFromPackage.put("com.microsoft.xboxone.smartglass", "logo-xbox");
+        iconFromPackage.put("com.microsoft.xboxone.smartglass.beta", "logo-xbox");
+        iconFromPackage.put("com.yahoo.mobile.client.android.mail", "logo-yahoo");
+        iconFromPackage.put("com.yahoo.mobile.client.android.search", "logo-yahoo");
+        iconFromPackage.put("com.yahoo.mobile.client.android.weather", "logo-yahoo");
+        iconFromPackage.put("com.yahoo.mobile.client.android.atom", "logo-yahoo");
+        iconFromPackage.put("com.yahoo.mobile.client.android.im", "logo-yahoo");
+        iconFromPackage.put("com.yahoo.mobile.client.android.finance", "logo-yahoo");
+        iconFromPackage.put("com.yahoo.mobile.client.android.sportacular", "logo-yahoo");
+        iconFromPackage.put("com.google.android.youtube", "logo-youtube");
+        iconFromPackage.put("com.tinder", "md-flame");
+        iconFromPackage.put("com.google.android.gm", "ios-mail");
+        iconFromPackage.put("com.sec.android.app.music", "ios-musical-notes");
+        iconFromPackage.put("com.google.android.music", "ios-musical-notes");
+        iconFromPackage.put("com.spotify.music", "ios-musical-notes");
+        iconFromPackage.put("org.telegram.messenger", "ios-paper-plane");
+        iconFromPackage.put("com.runtastic.android", "ios-walk");
+        iconFromPackage.put("com.runtastic.android.pro2", "ios-walk");
+        iconFromPackage.put("com.google.android.apps.photos", "ios-images");
+        iconFromPackage.put("com.google.android.apps.docs.editors.docs", "ios-document");
+        iconFromPackage.put("com.google.android.videos", "ios-film");
+        iconFromPackage.put("com.google.android.talk", "ios-quote");
+        iconFromPackage.put("com.google.android.apps.giant", "md-analytics");
+        iconFromPackage.put("com.google.android.apps.maps", "ios-map");
+        iconFromPackage.put("com.google.android.calendar", "ios-calendar");
+        iconFromPackage.put("com.google.android.contacts", "ios-contacts");
+        iconFromPackage.put("com.facebook.orca", "ios-text");
+        iconFromPackage.put("com.google.android.apps.messaging", "ios-text");
+        iconFromPackage.put("com.jb.gosms", "ios-text");
+        iconFromPackage.put("com.android.mms", "ios-text");
+        iconFromPackage.put("com.sonyericsson.conversations", "ios-text");
+        iconFromPackage.put("com.android.vending", "md-appstore");
+        iconFromPackage.put("com.google.android.dialer", "ios-call");
     }
 
     @Override
