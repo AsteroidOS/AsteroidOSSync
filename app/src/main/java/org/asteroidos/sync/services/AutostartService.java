8f716463acee907a1364b209c50668e452b832a7
Florent Revest <revestflo@gmail.com>
2018-03-11 17:04:28 +0100

    Add a basic AutostartService that automatically launches the SynchronizationService on Android's boot. Needs more work to actually connect to the watch automatically

--- app/src/main/java/org/asteroidos/sync/services/AutostartService.java
+++ app/src/main/java/org/asteroidos/sync/services/AutostartService.java
@@ -1,8 +1,21 @@
 package org.asteroidos.sync.services;
 
-/**
- * Created by kido on 06/03/18.
- */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
-class AutostartService {
+import org.asteroidos.sync.MainActivity;
+
+public class AutostartService extends BroadcastReceiver {
+    public void onReceive(Context context, Intent arg1)
+    {
+        SharedPreferences prefs = context.getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE);
+        String defaultDevMacAddr = prefs.getString(MainActivity.PREFS_DEFAULT_MAC_ADDR, "");
+
+        if(defaultDevMacAddr.length() > 0) {
+            Intent mSyncServiceIntent = new Intent(context, SynchronizationService.class);
+            context.startService(mSyncServiceIntent);
+        }
+    }
 }
