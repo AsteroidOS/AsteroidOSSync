76b3f06d0fa5e6fe4fc7d867b18995e3cb75434c
Florent Revest <revestflo@gmail.com>
2018-04-05 23:35:12 +0200

    PermissionsActivity: Remove an unnecessary import

--- app/src/main/java/org/asteroidos/sync/PermissionsActivity.java
+++ app/src/main/java/org/asteroidos/sync/PermissionsActivity.java
@@ -11,7 +11,6 @@ import android.os.PowerManager;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
