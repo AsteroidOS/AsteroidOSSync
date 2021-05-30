2127a1896544050ac61c19fe52566aecb051412b
Justus Tartz <git@jrtberlin.de>
2021-05-30 23:33:15 +0200

    add Signal to the ongoing notification allowlist
    This enables the display of incoming Signal calls.

--- app/src/main/java/org/asteroidos/sync/services/NLService.java
+++ app/src/main/java/org/asteroidos/sync/services/NLService.java
@@ -159,7 +159,7 @@ public class NLService extends NotificationListenerService {
         Notification notification = sbn.getNotification();
         String packageName = sbn.getPackageName();
 
-        String[] allowedOngoingApps = {"com.google.android.apps.maps"};
+        String[] allowedOngoingApps = {"com.google.android.apps.maps", "org.thoughtcrime.securesms"};
         if((notification.priority < Notification.PRIORITY_DEFAULT) ||
            ((notification.flags & Notification.FLAG_ONGOING_EVENT) != 0
             && !Arrays.asList(allowedOngoingApps).contains(packageName)) ||
