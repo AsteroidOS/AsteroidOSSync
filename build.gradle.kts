f3dfefe63bd34c8a72c3284aa42bb728ef32a4c0
doomsdayrs <doomsdayrs@gmail.com>
2021-06-26 12:14:07 -0400

    switch gradle DSL from groovy to kts
    - Converted settings.gradle
    - Converted root build.gradle
    - Converted app/build.gradle
    - Added kotlin gradle plugin
    - disable lint for missing translations
    
    Co-authored-by: jrtberlin <git@jrtberlin.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
     repositories {
         jcenter()
-        maven { url 'https://maven.google.com' }
+        maven("https://maven.google.com")
         google()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.1.3'
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath(kotlin("gradle-plugin:1.5.10"))
     }
 }
 
