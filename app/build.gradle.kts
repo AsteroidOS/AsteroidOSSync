870ab0d5a303cf7b694a316bc70a1edd162082ff
Justus Tartz <git@jrtberlin.de>
2021-10-05 13:27:35 +0200

    update material design to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("github.vatsal.easyweather:library:1.0.0")
     implementation("com.google.code.gson:gson:2.8.8")
     implementation("org.osmdroid:osmdroid-android:6.1.6")
