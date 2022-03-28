b0646251bcd7ed319bcbef8a28a31be6dadc0212
doomsdayrs <doomsdayrs@gmail.com>
2022-03-28 00:18:43 -0400

    Update kotlin gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.2")
-        classpath(kotlin("gradle-plugin:1.5.10"))
+        classpath(kotlin("gradle-plugin:1.6.10"))
     }
 }
 
