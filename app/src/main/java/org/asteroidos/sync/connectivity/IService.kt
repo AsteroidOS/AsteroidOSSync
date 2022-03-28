424ba34cb49e0fec0e5a13d9d92780cf5c80587c
doomsdayrs <doomsdayrs@gmail.com>
2022-03-28 00:33:46 -0400

    Convert IService to Kotlin
    Exceedingly easy

--- app/src/main/java/org/asteroidos/sync/connectivity/IService.kt
+++ app/src/main/java/org/asteroidos/sync/connectivity/IService.kt
@@ -1,14 +1,14 @@
-package org.asteroidos.sync.connectivity;
+package org.asteroidos.sync.connectivity
 
 /**
- * Every Service of Sync has to implement the {@link IService} interface.
+ * Every Service of Sync has to implement the [IService] interface.
  * A service is a module that either processes information that is exchanged
  * with the watch or is interested in knowing when the watch is connected and
  * when the watch is disconnected,
- * ({@link IService#sync()}) is called when the watch is connected.
- * ({@link IService#unsync()}) is called when the watch is disconnected.
+ * ([IService.sync]) is called when the watch is connected.
+ * ([IService.unsync]) is called when the watch is disconnected.
  */
-public interface IService {
-    public void sync();
-    public void unsync();
+interface IService {
+    fun sync()
+    fun unsync()
 }
