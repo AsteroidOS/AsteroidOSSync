52b5466303c00753d51d94a69a86a597c67d25a3
Justus Tartz <git@jrtberlin.de>
2022-09-18 20:25:53 +0200

    upgrade osmdroid to 6.1.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("com.github.code-crusher:EasyWeather:1.2")
     implementation("com.google.code.gson:gson:2.9.1")
-    implementation("org.osmdroid:osmdroid-android:6.1.6")
+    implementation("org.osmdroid:osmdroid-android:6.1.14")
     implementation("no.nordicsemi.android.support.v18:scanner:1.6.0")
     implementation("no.nordicsemi.android:ble:2.5.1")
 }
