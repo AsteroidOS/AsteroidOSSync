ad474edacdcd431741a7b966cd6e4791381c4f42
doomsdayrs <doomsdayrs@gmail.com>
2023-02-06 18:19:36 -0500

    mDetailFragment = not null in MainActivity.onDefaultDeviceSelected

--- app/src/main/java/org/asteroidos/sync/MainActivity.java
+++ app/src/main/java/org/asteroidos/sync/MainActivity.java
@@ -193,7 +193,7 @@ public class MainActivity extends AppCompatActivity implements DeviceListFragmen
         mDetailFragment = new DeviceDetailFragment();
 
         if (mListFragment != null) mListFragment.scanningStopped();
-        else if (mDetailFragment != null) mDetailFragment.scanningStopped();
+        else mDetailFragment.scanningStopped();
 
         getSupportFragmentManager()
                 .beginTransaction()
